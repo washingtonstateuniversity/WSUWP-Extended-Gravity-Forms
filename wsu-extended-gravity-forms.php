baf34490dcb4e825022b09fed25214c594f6b572
jeremyfelt <jeremy.felt@gmail.com>
2014-07-10 10:51:14 -0700

    Add initial files

---
+++ wsu-extended-gravity-forms.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: WSU Extended Gravity Forms
+Version: 0.1.0
+Plugin URI: http://web.wsu.edu
+Description: Extends and modifies default functionality in Gravity Forms.
+Author: washingtonstateuniversity, jeremyfelt
+Author URI: http://web.wsu.edu
+*/
+
+/**
+ * Class WSU_Extended_Gravity_Forms
+ */
+class WSU_Extended_Gravity_Forms {
+	/**
+	 * Setup hooks.
+	 */
+	public function __construct() {
+
+	}
+}
+$wsu_extended_gravity_forms = new WSU_Extended_Gravity_Forms();
