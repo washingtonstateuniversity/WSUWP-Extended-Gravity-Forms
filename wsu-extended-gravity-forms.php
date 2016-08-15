373441177de8b704e477a5593139fefd77c461f7
Jeremy Felt <jeremy.felt@gmail.com>
2016-08-15 16:22:28 -0700

    Bump version 0.4.0

--- wsu-extended-gravity-forms.php
+++ wsu-extended-gravity-forms.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSU Extended Gravity Forms
-Version: 0.3.0
+Version: 0.4.0
 Plugin URI: https://web.wsu.edu/
 Description: Extends and modifies default functionality in Gravity Forms.
 Author: washingtonstateuniversity, jeremyfelt
